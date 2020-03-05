47ae8efdfb264efb6919613c07ed04f019622edc
Alex Ho <alexanderkho92@gmail.com>
2020-03-05 15:49:59 -0700

    added GET route to server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./build'));
+const path = require('path');
+const buildPath = path.join(__dirname, '.', 'build');
+app.use(express.static(buildPath));
+console.log(buildPath)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+}); 
 const port = process.env.PORT || 1234;
 app.listen(1234);
 console.log(`listening on port ${port}`);
