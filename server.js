c9eb290ca900aacb1a33ef7febe6fa5b293afeef
Alex Ho <alexanderkho92@gmail.com>
2020-03-05 16:10:22 -0700

    remove GET route from server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 const path = require('path');
 const buildPath = path.join(__dirname, '.', 'build');
 app.use(express.static(buildPath));
-console.log(buildPath)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(buildPath, 'index.html'));
-}); 
 const port = process.env.PORT || 1234;
 app.listen(port);
 console.log(`listening on port ${port}`);
