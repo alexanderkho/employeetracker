0d69ac76e00d9ffeb11b517b9e534d656aa1350b
Alex Ho <alexanderkho92@gmail.com>
2020-03-05 16:16:19 -0700

    rollback to include GET route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const buildPath = path.join(__dirname, '.', 'build');
 app.use(express.static(buildPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+}); 
 const port = process.env.PORT || 1234;
 app.listen(port);
 console.log(`listening on port ${port}`);
