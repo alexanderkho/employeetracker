16a9b47abb28ab4fc3a0b3a939f22bda0a9b6536
Alex Ho <alexanderkho92@gmail.com>
2020-03-05 22:58:36 -0700

    remove middleware from Redux store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
 import rootReducer from './rootReducer.js';
-const store = createStore(rootReducer, middleware);
+const store = createStore(rootReducer);
 export default store;
